94f49641dede0cd02fbcd429d0c5632f6c8712d5
Shamli Ingole <54771718+shamli1997@users.noreply.github.com>
2024-04-16 00:51:14 -0700

    set up react query code

--- src/components/ui/FileUpload.tsx
+++ src/components/ui/FileUpload.tsx
@@ -1,12 +1,31 @@
 "use client";
 import { uploadToS3 } from "@/lib/db/s3";
-import { Inbox } from "lucide-react";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+import { Inbox, Loader2 } from "lucide-react";
 import React from "react";
 import { useDropzone } from "react-dropzone";
+import { toast } from "react-hot-toast";
 
 type Props = {};
 
 const FileUpload = () => {
+  const [uploading, setUploading] = React.useState(false);
+  const { mutate } = useMutation({
+    mutationFn: async ({
+      file_key,
+      file_name
+    }: {
+      file_key: string;
+      file_name: string;
+    }) => {
+      const response = await axios.post("/api/create-chat", {
+        file_key,
+        file_name
+      });
+      return response.data;
+    }
+  });
   const { getRootProps, getInputProps } = useDropzone({
     accept: { "application/pdf": [".pdf"] },
     maxFiles: 1,
@@ -14,14 +33,29 @@ const FileUpload = () => {
       console.log(acceptedFiles);
       const file = acceptedFiles[0];
       if (file.size > 10 * 1024 * 1024) {
-        alert("Upload file less than 10Mb");
+        toast.error("Upload file less than 10Mb");
         return;
       }
       try {
+        setUploading(true);
         const data = await uploadToS3(file);
         console.log(data);
+        if (!data?.file_key || !data.file_name) {
+          toast.error("Something went wrong");
+          return;
+        }
+        mutate(data, {
+          onSuccess: (data) => {
+            toast.success(data.message);
+          },
+          onError: (err) => {
+            toast.error("Error creating the chat!");
+          }
+        });
       } catch (error) {
         console.log(error);
+      } finally {
+        setUploading(false);
       }
     }
   });
@@ -34,10 +68,17 @@ const FileUpload = () => {
         })}
       >
         <input {...getInputProps()} />
-        <>
-          <Inbox className="w-10 h-10 text-blue-500" />
-          <p className="mt-2 text-sm text-slate-400">Upload PDF Here</p>
-        </>
+        {uploading ? (
+          <>
+            <Loader2 className="h-10 w-10 text-blue-500 animate-spin" />
+            <p className="mt-2 text-sm text-slate-400">Uploading your pdf...</p>
+          </>
+        ) : (
+          <>
+            <Inbox className="w-10 h-10 text-blue-500" />
+            <p className="mt-2 text-sm text-slate-400">Upload PDF Here</p>
+          </>
+        )}
       </div>
     </div>
   );
